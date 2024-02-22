4d208ea6a9694cb5ac65cc74bf88cb2732b07fe4
Antoine DC <antoinedeconto@gmail.com>
2024-02-22 17:38:40 +0100

    add router

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const express = require("express")
 // On crée l'application Express
 const app = express()
 const mongoose = require("mongoose")
-const Logements = require("./models/Logements.js")
+
+// On importe toutes nos routes
+const logementsRoutes = require('./routes/logements.js')
 
 app.use(express.json())
 
@@ -23,51 +25,9 @@ mongoose
   .then(() => console.log("Connexion à MongoDB réussie !"))
   .catch(() => console.log("Connexion à MongoDB échouée !"))
 
-// Express utilisera cette fonction pour tout type de requête
-// On renvoi une réponse en JSON
-app.get("/api/logements", (req, res, next) => {
-  Logements.find()
-    .then((logements) => res.status(200).json(logements))
-    .catch((error) => res.status(400).json({ error }))
-})
+  // A la place des anciens app.use()
+  app.use('/api/logements',logementsRoutes)
 
-app.post("/api/logements", (req, res, next) => {
-  const logement = new Logements({
-    ...req.body, // On décompose toutes les données dans le req.body
-  })
-  logement
-    .save() // On enregistre dans la BDD
-    .then(() => res.status(201).json({ message: "Le logement vient d'être créé !" }))
-    .catch((error) => res.status(400).json({ error: error }))
-})
-
-app.get("/api/logements/:id", (req, res, next) => {
-  Logements.findOne({ _id: req.params.id })
-    .then((logements) => {
-      console.log(logements)
-      res.status(200).json({
-        id: logement._id,
-        title: logements.title,
-        cover: "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-2-1.jpg",
-        pictures: [
-          "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-2-1.jpg",
-          "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-2-2.jpg",
-          "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-2-3.jpg",
-          "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-2-4.jpg",
-        ],
-        description: logements.description,
-        host: {
-          name: "Franck Maher",
-          picture: "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/profile-picture-2.jpg",
-        },
-        rating: "3",
-        location: "Ile de France - Paris 20e",
-        equipments: ["Wi-fi", "Cuisine équipée", "Télévision", "Sèche Cheveux"],
-        tags: ["Buttes Chaumont", "Laumière", "Studio"],
-      })
-    })
-    .catch((error) => res.status(404).json({ error }))
-})
 
 // On export l’application
 module.exports = app
